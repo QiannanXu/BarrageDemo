e98da6bd423b9f96d5d8a09efae0cb0a2075c9f1
QiannanXu <qnxu@thoughtworks.com>
2016-11-24 11:11:52 +0800

    Init commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button bsStyle='primary'>This is a button</Button>
+    </div>
+  )
+};
+
+export default HelloWorld;
